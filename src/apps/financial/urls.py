a3adad9d4408a74b666d6b5f735096225c3411d0
Miladimos <milad.jafari6210@gmail.com>
2026-08-19 00:41:29 +0330

    s

--- src/apps/financial/urls.py
+++ src/apps/financial/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from .views import callback_gateway_view, go_to_gateway_view
 
-app_name = 'apps.financial'
+app_name = "apps.financial"
 
 urlpatterns = [
-    path('pay/', go_to_gateway_view, name='pay'),
-    path('callback/', callback_gateway_view, name='callback_gateway'),
+    path("pay/", go_to_gateway_view, name="pay"),
+    path("callback/", callback_gateway_view, name="callback_gateway"),
 ]
